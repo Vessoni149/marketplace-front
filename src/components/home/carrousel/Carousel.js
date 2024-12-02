748a5bf10c1f4106af8e7840b0a638d4494ef598
Agustin Vessoni <agustinvessoni149@gmail.com>
2024-12-02 09:51:43 -0600

    fix-Carrousel & productCartItem styles modified

--- src/components/home/carrousel/Carousel.js
+++ src/components/home/carrousel/Carousel.js
@@ -17,6 +17,8 @@ const Carousel = () => {
         speed: 500,
         slidesToShow: 1,
         slidesToScroll: 1,
+        autoplay:true,
+        autoplaySpeed:4000,
         arrows: true,
     };
 
